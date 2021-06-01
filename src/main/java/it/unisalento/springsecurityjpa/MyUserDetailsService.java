4926645a772888af7648a5d47adea36dc24029c5
Lamberto Preite <78571193+Lamberto31@users.noreply.github.com>
2021-06-01 19:07:17 +0200

    Configurato collegamento Spring Security e JPA tramite MyUserDetails e MyUserDetailsService

--- src/main/java/it/unisalento/springsecurityjpa/MyUserDetailsService.java
+++ src/main/java/it/unisalento/springsecurityjpa/MyUserDetailsService.java
@@ -1,15 +1,27 @@
 package it.unisalento.springsecurityjpa;
 
 import it.unisalento.springsecurityjpa.models.MyUserDetails;
+import it.unisalento.springsecurityjpa.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return new MyUserDetails(s);
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUserName(username);
+
+        user.orElseThrow(() -> new UsernameNotFoundException("Not found: " + username));
+
+        return user.map(MyUserDetails::new).get();
     }
 }
